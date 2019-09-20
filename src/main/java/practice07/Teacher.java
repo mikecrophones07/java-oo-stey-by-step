e6662603ecede3d44b4a6305c61448ef38e95d71
Mike Anthony A. Echon <mike.anthony.echon@oocl.com>
2019-09-20 19:28:58 +0800

    practice07 - Pass test6 and extend Person class to Teacher class and change third parameter to object parameter and add getKlass that return object

--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,16 @@
 package practice07;
 
-public class Teacher {
+import practice04.Person;
+
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(String name, Integer age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass(){
+        return klass;
+    }
 }
