75f5dac2c3d07ff47504e8e689a7029c2454924e
Mike Anthony A. Echon <mike.anthony.echon@oocl.com>
2019-09-20 22:52:22 +0800

    practice11 - Pass almost all of the test

--- src/main/java/practice11/Student.java
+++ src/main/java/practice11/Student.java
@@ -1,4 +1,28 @@
 package practice11;
 
-public class Student {
+public class Student extends Person {
+
+    private Klass klass;
+    private Integer id;
+
+    public Student(Integer id, String name, Integer age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+        this.id = id;
+    }
+
+    public Klass getKlass(){
+        return klass;
+    }
+
+    @Override
+    public String introduce(){
+        StringBuilder tempStr = new StringBuilder();
+        if(klass.getLeader() != null){
+            return tempStr.append(super.introduce()).append(" I am a Student. I am Leader of ").append(klass.getDisplayName()).append(".").toString();
+        }
+        else {
+            return tempStr.append(super.introduce()).append(" I am a Student. I am at ").append(klass.getDisplayName()).append(".").toString();
+        }
+    }
 }
