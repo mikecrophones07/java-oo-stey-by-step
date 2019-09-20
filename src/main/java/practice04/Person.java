333cbc692e9b1cb8cfb526d650a4a7e5b170d349
Mike Anthony A. Echon <mike.anthony.echon@oocl.com>
2019-09-20 18:33:32 +0800

    practice04 - Pass test1 and add constructor and getter from Person class

--- src/main/java/practice04/Person.java
+++ src/main/java/practice04/Person.java
@@ -1,4 +1,20 @@
 package practice04;
 
 public class Person {
+
+    private String name;
+    private Integer age;
+
+    public Person(String name, Integer age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public Integer getAge(){
+        return age;
+    }
 }
