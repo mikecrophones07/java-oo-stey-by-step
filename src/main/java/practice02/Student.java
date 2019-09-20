f54f5694636855b42e6625b716c11c9edcf4e1d2
Mike Anthony A. Echon <mike.anthony.echon@oocl.com>
2019-09-20 18:17:29 +0800

    practice02 - Pass test3 and Add getter named klass in Person class

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,25 @@
 package practice02;
 
 public class Student {
+    private String name;
+    private Integer klass;
+    private Integer age;
+
+    public Student(String name, Integer age, Integer klass){
+        this.name = name;
+        this.age = age;
+        this.klass = klass;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public Integer getAge(){
+        return age;
+    }
+
+    public Integer getKlass(){
+        return klass;
+    }
 }
