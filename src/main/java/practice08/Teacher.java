bd489a4585c210338a756634f2eed7362a773ee5
Mike Anthony A. Echon <mike.anthony.echon@oocl.com>
2019-09-20 20:27:08 +0800

    practice08 - Pass test10 and add teacher constructor and getters

--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,16 @@
 package practice08;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+    private Integer id;
+
+    public Teacher(Integer id, String name, Integer age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+        this.id = id;
+    }
+
+    public Klass getKlass(){
+        return klass;
+    }
 }
