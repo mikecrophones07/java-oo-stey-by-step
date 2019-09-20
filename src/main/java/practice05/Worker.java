9119dad36ef125dd016be19f6efaa49f070b00df
Mike Anthony A. Echon <mike.anthony.echon@oocl.com>
2019-09-20 18:54:38 +0800

    practice05 - Pass test5 and extend Person class from Worker class

--- src/main/java/practice05/Worker.java
+++ src/main/java/practice05/Worker.java
@@ -1,4 +1,10 @@
 package practice05;
 
-public class Worker {
+import practice04.Person;
+
+public class Worker extends Person {
+
+    public Worker(String name, Integer age) {
+        super(name, age);
+    }
 }
