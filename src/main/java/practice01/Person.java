988210ef613935ee34521cbbba2b0f8c0952d35e
Mike Anthony A. Echon <mike.anthony.echon@oocl.com>
2019-09-20 18:06:50 +0800

    Pass should_person_have_name_and_age test & Edit Person Class with Function

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -1,4 +1,21 @@
 package practice01;
 
+import java.util.PrimitiveIterator;
+
 public class Person {
+    private String name;
+    private Integer age;
+
+    public Person(String name, Integer age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public Integer getAge(){
+        return age;
+    }
 }
