ab29e62e57dfc9fbfd63a9720d25a3fd1bfa009f
Mike Anthony A. Echon <mike.anthony.echon@oocl.com>
2019-09-20 20:34:51 +0800

    practice08 - Pass test11-13

--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,5 +1,6 @@
 package practice08;
 
+
 public class Teacher extends Person {
     private Klass klass;
     private Integer id;
@@ -10,7 +11,33 @@ public class Teacher extends Person {
         this.id = id;
     }
 
+    public Teacher(Integer id, String name, Integer age){
+        super(name, age);
+        this.id = id;
+    }
+
     public Klass getKlass(){
         return klass;
+    }
+
+    @Override
+    public String introduce(){
+        StringBuilder tempStr = new StringBuilder();
+        if(klass == null){
+            return tempStr.append(super.introduce()).append(" I am a Teacher. I teach No Class.").toString();
+        }
+        else {
+            return tempStr.append(super.introduce()).append(" I am a Teacher. I teach ").append(klass.getDisplayName()).append(".").toString();
+        }
+    }
+
+    public String introduceWith(Student student){
+        StringBuilder tempStr = new StringBuilder();
+        if(klass.getNumber() != student.getKlass().getNumber()) {
+            return tempStr.append(super.introduce()).append(" I am a Teacher. I don't teach ").append(student.getName()).append(".").toString();
+        }
+        else{
+            return tempStr.append(super.introduce()).append(" I am a Teacher. I teach ").append(student.getName()).append(".").toString();
+        }
     }
 }
