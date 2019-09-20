62772b35a13da118b4d4a134fcfa12480d7cdafc
Mike Anthony A. Echon <mike.anthony.echon@oocl.com>
2019-09-20 18:43:33 +0800

    practice04 - Pass test6 and add introduce class from Worker class and call introduce from super class and append it to the return of introduce class from Worker class

--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -5,4 +5,10 @@ public class Worker extends Person{
     public Worker(String name, Integer age) {
         super(name, age);
     }
+
+    @Override
+    public String introduce(){
+        StringBuilder tempStr = new StringBuilder();
+        return tempStr.append(super.introduce()).append(" I am a Worker. I have a job.").toString();
+    }
 }
