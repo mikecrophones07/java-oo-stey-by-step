99d3e9582cbef930d8dd57ce6c5b561b9251859c
Mike Anthony A. Echon <mike.anthony.echon@oocl.com>
2019-09-20 18:45:59 +0800

    practice05 - Pass test3 and extend Person class to Student class and add function named getKlass from Student class

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,16 @@
 package practice05;
 
-public class Student {
+import practice04.Person;
+
+public class Student extends Person {
+    private Integer klass;
+
+    public Student(String name, Integer age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Integer getKlass(){
+        return klass;
+    }
 }
