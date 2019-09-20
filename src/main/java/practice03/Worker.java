51d13af867ec389d4ccf930cdc848fd3df3d04cf
Mike Anthony A. Echon <mike.anthony.echon@oocl.com>
2019-09-20 18:31:16 +0800

    practice03 - Pass test6 and override introduce function from Person Class to Worker Class

--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -1,7 +1,14 @@
 package practice03;
 
 public class Worker extends Person{
+
     public Worker(String name, Integer age) {
         super(name, age);
+    }
+
+    @Override
+    public String introduce(){
+        StringBuilder tempStr = new StringBuilder();
+        return tempStr.append("I am a Worker. I have a job.").toString();
     }
 }
