2903bfefd5a0c8b4919fac1e43806922fb777e1c
Mike Anthony A. Echon <mike.anthony.echon@oocl.com>
2019-09-20 18:23:03 +0800

    practice03 - Pass test1 and Add constructor and getters in Person Class

--- src/main/java/practice03/Person.java
+++ src/main/java/practice03/Person.java
@@ -1,4 +1,24 @@
 package practice03;
 
 public class Person {
+    private String name;
+    private Integer age;
+
+    public Person(String name, Integer age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public Integer getAge(){
+        return age;
+    }
+
+    public String introduce(){
+        StringBuilder tempStr = new StringBuilder();
+        return tempStr.append("My name is ").append(name).append(". I am ").append(age).append(" years old.").toString();
+    }
 }
