41d16af746d8952dd4374c7904012cde4161cf14
Mike Anthony A. Echon <mike.anthony.echon@oocl.com>
2019-09-20 18:29:26 +0800

    practice03 - Pass test5 and extend Person class from Worker Class

--- src/test/java/practice03/Practice03Test.java
+++ src/test/java/practice03/Practice03Test.java
@@ -32,13 +32,13 @@ public class Practice03Test {
         Student tom = new Student("Tom", 21, 2);
         assertThat(tom.introduce()).isEqualTo("I am a Student. I am at Class 2.");
     }
-//
-//    @Test
-//    public void should_worker_have_name_and_age() throws Exception {
-//        Worker tom = new Worker("Tom", 21);
-//        assertThat(tom.getName()).isEqualTo("Tom");
-//        assertThat(tom.getAge()).isEqualTo(21);
-//    }
+
+    @Test
+    public void should_worker_have_name_and_age() throws Exception {
+        Worker tom = new Worker("Tom", 21);
+        assertThat(tom.getName()).isEqualTo("Tom");
+        assertThat(tom.getAge()).isEqualTo(21);
+    }
 //
 //    @Test
 //    public void should_worker_introduce_itself_with_no_field() throws Exception {
